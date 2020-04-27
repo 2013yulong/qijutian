37766a39aa366eab8689e26ea6c703e35bc2c265
Marx <yulongjia209722@sohu-inc.com>
2020-04-27 14:50:54 +0800

    music my seat 3d

---
+++ learning.js
@@ -0,0 +1,21 @@
+// learning.js
+"如何做人物报道.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/8adb3dda-25ed-49e4-b569-75a4fe066fb7.mp4"
+"《初级视频的拍摄技巧》洪亮亮2.25.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/02c1a344-5ad1-42e8-a17b-007e4f65deee.mp4"
+"《从稿件推荐到选题与制作表达》方晨露3.18.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/957dacc2-f512-4d93-8ee1-75a277536060.mp4"
+"3.24用Go语言重构图片上传服务实践.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/9ed527bf-074e-405a-9e68-97f2fdc89b85.mp4"
+"网易新闻监控系统的设计与应用龙猫监控平台从0到李春阳.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/46ba7624-f694-4756-a0c0-7de154e26cbe.mp4"
+"【AI】广告图像识别方案.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/919b4c53-8a90-4d29-9ed0-a2cc99a00319.mp4"
+"爆款引流训练营养成记.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/2ea61212-e138-4e20-a89b-2dc9b0878bcc.mp4"
+"解决方案人员的提升之路.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/2a62ab32-83f3-4a93-b40f-ddde6bec0bf2.mp4"
+"【设计】利用中台思维提升设计效率和质量.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/ef7daa3f-5937-48f2-bba1-c8e54b162cdb.mp4"
+"全局性设计思维.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/f707fa10-5888-42bd-a728-ec28776e1f1b.mp4"
+"【设计】copy一款产品的思路与落地设计.mp4：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/ae909df6-c1f8-4752-9b8c-ee4ec953668a.mp4"
+"【实践者论坛】从UED到UGD：用设计思维做增长.mp4:http://vodn0hku7xy.vod.126.net/vodn0hku7xy/1a02cff4-cbc4-4f7d-bce1-a8b99317813b.mp4"
+"从UED到UGD：用设计思维做增长：https://vodn0hku7xy.vod.126.net/vodn0hku7xy/1a02cff4-cbc4-4f7d-bce1-a8b99317813b.mp4"
+'用户增长心得：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/4291a85f-5840-4d06-864c-ded05251c548.mp4'
+'设计师如何做到“够专业”：https://vodn0hku7xy.vod.126.net/vodn0hku7xy/423e9be3-a416-4378-b648-12af91158766.mp4'
+'人工智能】深度学习中的知识蒸馏技术：https://vodn0hku7xy.vod.126.net/vodn0hku7xy/ee58aa1c-fd3c-4652-81a4-8c51e687608d.mp4'
+'云信音视频录制服务测试实践：http://vodn0hku7xy.vod.126.net/vodn0hku7xy/af399bf4-2295-4e22-9f93-7c39222a4804.mp4'
+
+
+
